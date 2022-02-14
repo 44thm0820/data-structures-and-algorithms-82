4f7189d647ee141d1aa24be8b9daf3a07abeffad
Rey Mercado <reymercado.usa@gmail.com>
2022-02-14 15:44:41 -0800

    challenge6 passed

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -116,6 +116,9 @@ This function should use forEach to populate your grocery list based on the stor
 
 const createList = (availableItems) => {
   // Solution code here...
+  let arr = [];
+  availableItems.forEach(fruitItem => fruitItem.available ? arr.push(fruitItem.name) : null);
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
