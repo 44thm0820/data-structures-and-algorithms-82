1ee5aa23da5b21238db13580a00a212be4ffcb82
Rey Mercado <reymercado.usa@gmail.com>
2022-02-23 18:33:49 -0800

    challenges-08 complete without stretch goals

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -131,6 +131,7 @@ Write a function named citiesAtoJ that takes in an array of city names and uses 
 
 const citiesAtoJ = (arr) => {
   // Solution code here...
+  return arr.filter(cityName => cityName.match(/(^[A-J])/));
 };
 
 /* ------------------------------------------------------------------------------------------------
