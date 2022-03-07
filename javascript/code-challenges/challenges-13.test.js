bb671a7e0557f21f7ae07aa5328596f181165610
Rey Mercado <reymercado.usa@gmail.com>
2022-03-07 06:25:29 -0800

    codechallenge13 complete

--- javascript/code-challenges/challenges-13.test.js
+++ javascript/code-challenges/challenges-13.test.js
@@ -103,6 +103,7 @@ Write a function named allHappy that takes in an array of strings and returns a 
 
 const allHappy = (arr) => {
   // Solution code here...
+  return arr.every(str => str.includes(':)'));
 };
 
 /* ------------------------------------------------------------------------------------------------
