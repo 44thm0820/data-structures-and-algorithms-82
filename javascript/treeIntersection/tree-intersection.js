6fe5e46360fce9370e48a487a3c3484ecfe5110e
Rey Mercado <reymercado.usa@gmail.com>
2022-05-03 23:52:19 -0700

    first-commit wip tree-intersection

---
+++ javascript/treeIntersection/tree-intersection.js
@@ -0,0 +1,5 @@
+function tree_intersection (bt1, bt2) {
+  let values = [];
+  // Using your Hashmap implementation as a part of your algorithm, return a set of values found in both trees.
+  return values;
+}
